3a28d03d73f128dc4f5db6af1292a9032a0211bf
Sergo <prizrakgames21@gmail.com>
2026-01-06 11:00:59 +0400

    Fix: fix external database url support (postgres tested only)

--- app/db/database.py
+++ app/db/database.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine, Session, SQLModel
 
 from app.core.config import DB_URL
 
-engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DB_URL)
 
 
 def create_db_and_tables():
